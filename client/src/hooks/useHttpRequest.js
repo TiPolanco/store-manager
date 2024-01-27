00782ea0b7306829963536e1c4f2897e50cb3378
TiPolanco <timpolanco2@gmail.com>
2024-01-27 12:56:42 -0800

    create custom hook for making http request

---
+++ client/src/hooks/useHttpRequest.js
@@ -0,0 +1,67 @@
+import { useState, useCallback } from 'react';
+
+const API_ENDPOINT = 'http://localhost:8000';
+
+const useHttpRequest = ({
+    dataValidation,
+    method = 'GET',
+    onError,
+    onSuccess,
+    options,
+    url,
+}) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const makeRequest = useCallback(async (data) => {
+        // Data Validation
+        if (
+            typeof dataValidation === 'function' &&
+            !dataValidation(data)
+        ) {
+            const validationError = new Error('Invalid data.');
+            if (onError) onError(validationError);
+            setError(validationError);
+        }
+
+        setIsLoading(true);
+
+        // Compute request options
+        const requestOptions = {
+            credentials: "include", // Enable cookie for CORS
+            headers: { 'Content-Type': 'application/json' },
+            method,
+            ...(options || {}),
+        };
+        if (data) {
+            requestOptions.body = JSON.stringify(data);
+        }
+
+        try {
+            // Make request
+            const response = await fetch(`${API_ENDPOINT}${url}`, requestOptions);
+            const json = await response.json();
+
+            // Handle response
+            if (response.ok) {
+                onSuccess(json);
+            } else {
+                if (onError) onError(error);
+                setError(error);
+            }
+        } catch (error) {
+            if (onError) onError(error);
+            setError(error);
+        }
+
+        setIsLoading(true);
+    }, [dataValidation, method, onError, onSuccess, options, url]);
+
+    return {
+        error,
+        isLoading,
+        makeRequest,
+    };
+};
+
+export default useHttpRequest;
