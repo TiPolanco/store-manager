a37ea3ef4d6f9c5e71d411e73780ab51b4372348
TiPolanco <timpolanco2@gmail.com>
2024-01-27 21:45:46 -0800

    create api routes for /bookings

--- server/routes/storeRoutes.js
+++ server/routes/storeRoutes.js
@@ -1,6 +1,6 @@
 import dbPool from '../database/db.js';
 import auth from '../middleware/auth.js';
-import permission, { STORE_CREATE, STORE_EDIT } from '../middleware/permission.js';
+import permission, { BOOKING_CREATE, STORE_CREATE, STORE_EDIT } from '../middleware/permission.js';
 
 const BASE_ROUTE = "/api/stores";
 
@@ -61,4 +61,63 @@ export default function(app) {
     });
 
     // Bookings
+    // List all active bookings
+    // Accessible to everyone
+    app.get(`${BASE_ROUTE}/bookings`, async (req, res) => {
+        try {
+            const { rows } = await dbPool.query(`
+                SELECT public."Bookings".id, store_id, user_id, public."Bookings".desc, start_date, end_date, public."User".name AS user_name, public."User".pfp, public."Store".name AS store_name, public."Bookings".timestamp
+                    FROM (public."Bookings" INNER JOIN public."Store" ON public."Bookings".store_id = public."Store".id) INNER JOIN public."User" on public."User".id = public."Bookings".user_id
+                    WHERE public."Bookings".active = true AND public."Store".active = true AND public."User".active = true AND end_date > NOW() 
+                    ORDER BY public."Bookings".start_date ASC
+            `);
+            res.json(rows);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    });
+    
+    // Create new booking
+    // Requires STORE_CREATE permission
+    app.post(`${BASE_ROUTE}/bookings`, auth, permission(BOOKING_CREATE), async (req, res) => {
+        const { startDate, endDate, desc, storeID, userID } = req.body;
+
+        console.log({
+            startDate: typeof startDate,
+        })
+
+        try {
+            const { rows: users } = await dbPool.query(`
+                SELECT name, pfp FROM public."User" WHERE id = '${userID}' AND active = true
+            `);
+            if (!users.length) res.status(400).json({ message: `User id ${userID} does not exist.` });
+            
+            const { rows: stores } = await dbPool.query(`
+                SELECT name FROM public."Store" WHERE id = '${storeID}' AND active = true
+            `);
+            if (!stores.length) res.status(400).json({ message: `Store id ${storeID} does not exist.` });
+            console.log('QA: newBookings', users, stores);
+
+            const { rows: newBookings } = await dbPool.query(`
+                INSERT INTO public."Bookings"(
+                    store_id, user_id, "desc", start_date, end_date, active, "timestamp")
+                    VALUES ($1, $2, $3, $4, $5, $6, to_timestamp(${Date.now()} / 1000.0))
+                    RETURNING id, store_id, user_id, "desc", start_date, end_date, "timestamp"
+            `, [storeID, userID, desc, startDate, endDate, true]);
+            console.log('QA: newBookings', newBookings);
+
+            const data = {
+                ...newBookings[0],
+                pfp: users[0].pfp,
+                store_name: stores[0].name,
+                user_name: users[0].name,
+            }
+    
+            res.status(201).json(data);
+        } catch (err) {
+            console.log('QA: error', err);
+            res.status(500).json(err);
+        }
+    });
+
 };
